1a4e8a1739e75d651b88398842bd6632b9d2f081
Shuhei Nomura <info@danmaq.com>
2014-11-30 14:34:19 +0000

    Stateパターンの単体テスト更新。

--- test/State/ContextTest.php
+++ test/State/ContextTest.php
@@ -4,16 +4,25 @@ require_once 'SplClassLoader.php';
 $loader = new SplClassLoader('UnityCrash', 'lib/vendors');
 $loader->register();
 
+use UnityCrash\State\Context;
 use UnityCrash\State\EmptyState;
 
 class ContextTest extends PHPUnit_Framework_TestCase
 {
+
+	private $context;
+
 	public function testInstance()
 	{
-		$this->assertTrue(class_exists('Context'), 'クラスがないっぽい');
+		$this->assertInstanceOf('UnityCrash\State\IContext', new Context());
+		// ホワイトボックステストのみ
+		new Context(EmptyState::getInstance());
 	}
+	
+	// TODO: ここから
 	
 	protected function setUp()
 	{
+		$this->context = new Context();
 	}
 }
--- test/State/EmptyStateTest.php
+++ test/State/EmptyStateTest.php
@@ -11,7 +11,7 @@ use UnityCrash\Utils\Singleton;
 class EmptyStateTest extends PHPUnit_Framework_TestCase
 {
 	private $context;
-	
+
 	public function testInstance()
 	{
 		$this->assertTrue(class_exists('UnityCrash\State\EmptyState'), 'UnityCrash\State\EmptyState is not found');
@@ -23,23 +23,23 @@ class EmptyStateTest extends PHPUnit_Framework_TestCase
 	public function testSetup()
 	{
 		// ホワイトボックステストのみ
-		UnityCrash\State\EmptyState::getInstance()->setup($context);
+		UnityCrash\State\EmptyState::getInstance()->setup($this->context);
 	}
 
 	public function testLoop()
 	{
 		// ホワイトボックステストのみ
-		UnityCrash\State\EmptyState::getInstance()->loop($context);
+		UnityCrash\State\EmptyState::getInstance()->loop($this->context);
 	}
 
 	public function testTeardown()
 	{
 		// ホワイトボックステストのみ
-		UnityCrash\State\EmptyState::getInstance()->teardown($context);
+		UnityCrash\State\EmptyState::getInstance()->teardown($this->context);
 	}
 
 	protected function setUp()
 	{
-		$context = new Context();
+		$this->context = new Context();
 	}
 }
